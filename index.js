b433549997377160d82317db20b7769e4301561f
Bgsunil7951 <98315283+Bgsunil7951@users.noreply.github.com>
2023-09-07 21:23:02 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,25 @@
+function sendMail() {
+    var params = {
+        name: document.getElementById("name").value,
+        email: document.getElementById("email").value,
+        message: document.getElementById("message").value,
+        number: document.getElementById("number").value,
+    }
+const serviceID = "service_tlha0ur";
+const templateID ="template_pyoaf3e";
+
+emailjs
+.send(serviceID,templateID,params)
+.then(
+    res =>{
+        document.getElementById("name").value ="";
+        document.getElementById("email").value ="";
+        document.getElementById("number").value ="";
+        document.getElementById("message").value ="";
+        console.log(res);
+        alert("Mail send Successfully")
+    }
+)
+.catch((err) => console.log(err));
+}
+
